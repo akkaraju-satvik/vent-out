d97b451fe90482b3d144a982ac2f476fa599bef8
Akkaraju Satvik <satvik.akkaraju@gmail.com>
2021-09-03 10:54:51 +0530

    DB connection with ENV variables

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const publicPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicPath))
